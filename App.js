57aed2bfbe3af4e291589137a4a52399b92408da
MomenSherif <momensherif.2019@gmail.com>
2020-10-14 12:00:37 +0200

    ✨ NEW FEATURE: Products Navigator

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+
 import store from './redux/store';
+import ShopNavigator from './navigation/ShopNavigator';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <ShopNavigator />
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
-  },
-});
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import { FlatList } from 'react-native';
+import { Text, FlatList } from 'react-native';
+import { useSelector } from 'react-redux';
 
 const ProductsOverviewScreen = (props) => {
-  return <FlatList />
+  const products = useSelector(state => state.products.availableProducts);
+
+  return (
+    <FlatList
+      data={products}
+      keyExtractor={item => item.id}
+      renderItem={({ item }) => (
+        <Text>{item.title}</Text>
+      )}
+    />
+  );
 };
+
+ProductsOverviewScreen.navigationOptions = {
+  headerTitle: 'All Products',
+}
 
 export default ProductsOverviewScreen;
