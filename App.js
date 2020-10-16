42ae3b2b2c0cdc8e43cf4cf9ab288aa3985aab6e
MomenSherif <momensherif.2019@gmail.com>
2020-10-16 13:29:52 +0200

    🆕 NEW: Using Custom Fonts

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { AppLoading } from 'expo';
+import { useFonts } from 'expo-font';
 
 import store from './redux/store';
 import ShopNavigator from './navigation/ShopNavigator';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+
+  if (!fontsLoaded)
+    return <AppLoading />;
+
   return (
     <Provider store={store}>
       <ShopNavigator />
--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import { ScrollView, View, Text, Image, Button, StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
+import colors from '../../constants/colors';
 
 const ProductDetailScreen = ({ navigation }) => {
   const id = navigation.getParam('id');
   const product = useSelector(state => state.products.availableProducts.find(prod => prod.id === id));
 
   return (
-    <View>
-      <Text>{product.title}</Text>
-    </View>
+    <ScrollView>
+      <Image source={{ uri: product.imageUrl }} style={styles.image} />
+      <View style={styles.actions}>
+        <Button title="Add to Cart" color={colors.primary} onPress={() => console.log(`Adding ${product.title} to cart!`)} />
+      </View>
+      <Text style={styles.price}>${product.price.toFixed(2)}</Text>
+      <Text style={styles.description}>{product.description}</Text>
+    </ScrollView>
   );
 };
 
-ProductDetailScreen.navigationOptions = ({navigation}) => {
+ProductDetailScreen.navigationOptions = ({ navigation }) => {
   const title = navigation.getParam('title');
   return {
     headerTitle: title,
@@ -21,7 +27,27 @@ ProductDetailScreen.navigationOptions = ({navigation}) => {
 }
 
 const styles = StyleSheet.create({
-
+  image: {
+    width: '100%',
+    height: 300,
+  },
+  actions: {
+    marginVertical: 10,
+    alignItems: 'center',
+  },
+  price: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 20,
+    color: '#888',
+    textAlign: 'center',
+    marginVertical: 20,
+  },
+  description: {
+    fontFamily: 'open-sans',
+    fontSize: 14,
+    textAlign: 'center',
+    marginHorizontal: 20
+  },
 });
 
 export default ProductDetailScreen
